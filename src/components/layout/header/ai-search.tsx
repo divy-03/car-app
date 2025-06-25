9826fba2802ad84a3e2b96aaf172ca588f0a9e66
divy-03 <127951881+divy-03@users.noreply.github.com>
2025-06-25 23:17:08 +0530

    added seller page

--- src/components/layout/header/ai-search.tsx
+++ src/components/layout/header/ai-search.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -10,12 +11,16 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
-import { SearchIcon } from "lucide-react";
+import { findCar } from "@/lib/actions/ai-action";
+import { SearchIcon, SmileIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { string } from "zod";
 
 const AISearch = () => {
-  // const [isLoading, setIsLoading] = useState(false);
-  const isLoading = false; // Placeholder for loading state
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const [isOpen, setIsOpen] = useState(false);
   const [description, setDescription] = useState("");
@@ -36,36 +41,61 @@ const AISearch = () => {
 
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Your logic here
+    if (!description) return toast.error("Please enter some description first");
+
+    setIsLoading(true);
+
+    try {
+      const result: "No car found" | "Error generating car search" | string =
+        await findCar(description);
+
+      const carId = string().parse(result);
+      router.replace(`/cars/${carId}`);
+
+      toast.success(`Car found with ID: ${carId}`);
+      setDescription("");
+      setIsOpen(false);
+    } catch (error) {
+      if (error instanceof Error) toast.error(error.message);
+      else toast.error("An unexpected error occurred.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <Dialog onOpenChange={setIsOpen} open={isOpen}>
-      <DialogTrigger className="ml-auto mr-4 flex items-center gap-1 bg-muted rounded-lg px-4 py-2 hover:bg-muted/80 transition-colors duration-300">
-        <SearchIcon className="h-4 w-4" /> Search with AI &quot;CTRL+K&quot;
-        
+      <DialogTrigger className="ml-auto mr-4  flex items-center gap-1  bg-muted rounded-lg px-4 py-2 hover:bg-muted">
+        <SearchIcon className="h-4 w-4" /> Search with AI
+        <Badge>Ctrl+K</Badge>
       </DialogTrigger>
-
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Define what type of car you like</DialogTitle>
+          <DialogTitle>Define what type of car you like </DialogTitle>
           <DialogDescription>
-            You can tell features like color, type, and mode. For example, &quot;I like red sports cars with a convertible roof.&quot;
+            You can tell features like color, type, and model. For example: I
+            like a red SUV with a sunroof.
           </DialogDescription>
         </DialogHeader>
 
         <form className="flex flex-col gap-4" onSubmit={submitHandler}>
           <Textarea
-            placeholder="Write about your car preferences"
+            placeholder="Write about your dream car..."
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            className="w-full h-32 p-2 border border-gray-300 rounded-lg"
             rows={5}
+            className="max-h-[20rem]"
+            style={{
+              // @ts-expect-error: css not updated
+              fieldSizing: "content",
+            }}
           />
 
-          <Button disabled={isLoading} className="flex items-center gap-1">
-            {isLoading ? "Searching..." : "Search"}
-            <SearchIcon className="h-4 w-4" />
+          <Button
+            disabled={isLoading}
+            className="flex items-center gap-1 cursor-pointer"
+          >
+            {isLoading ? "Searching..." : <>Find my dream car</>}
           </Button>
         </form>
       </DialogContent>
